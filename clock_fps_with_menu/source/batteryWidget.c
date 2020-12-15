8996bcab7dd433ee436dab128aa28dac671f3740
lzgmc <lazyguymc@lzgmc.tk>
2020-12-15 14:05:16 -0800

    Restore AM/PM text
    This reverts commit 5e8b3dc140cd98b78df2de255466fd21b0260bb3.

--- clock_fps_with_menu/source/batteryWidget.c
+++ clock_fps_with_menu/source/batteryWidget.c
@@ -21,6 +21,8 @@ Result  MCU_GetBatteryLevel(u8* out);
 #define RED         255,0,0
 #define BLANK       255,255,255
 
+static char * g_speriod[] = { "AM", "PM"};
+
 int     g_clockWidgetPosX = 338;
 int     g_clockWidgetPosY = 11;
 int     g_clockWidth = 0;
@@ -102,6 +104,13 @@ int     DrawClockAndBattery(int isBottom, u32 percent, u32 clockType)
     u32 batteryX = xPos;
     u32 width = g_clockWidth = 57;
     u32 posY = g_clockWidgetPosY;
+    
+    // If it is a 12 hour clock, adjust things slightly
+    if (clockType == 0)
+    {
+        xPos = g_clockWidgetPosX - 12;
+        width = g_clockWidth = 69;
+    }
 
     //DrawBackground
     OvDrawTranspartBlackRect(ADJ(xPos, isBottom), posY - 2, width, 12, 1);
@@ -138,6 +147,8 @@ int     DrawClockAndBattery(int isBottom, u32 percent, u32 clockType)
     int period = GetTimeString(buf, clockType);
     OvDrawString(buf, ADJ(xPos, isBottom), posY, BLANK);
     xPos += 31;
+    if (clockType == 0)
+        OvDrawString(g_speriod[period], ADJ(xPos, isBottom), posY, BLANK);
 
     return (1);
 }
@@ -152,10 +163,19 @@ int     DrawClockOnly(int isBottom, u32 clockType)
     u32 xPos = g_clockWidgetPosX - 14;
     u32 width = g_clockWidth = 33;
     u32 posY = g_clockWidgetPosY;
+    
+    // If it is a 12 hour clock, adjust things slightly
+    if ( clockType == 0 )
+    {
+        xPos = g_clockWidgetPosX + 1;
+        width = g_clockWidth = 46;
+    }
 
     //DrawBackground
     OvDrawTranspartBlackRect(ADJ(xPos, isBottom), posY - 2, width, 12, 1);
     // Draw clock
     OvDrawString(buf, ADJ(xPos + 2, isBottom), posY, 255, 255, 255);
+    if (clockType == 0)
+        OvDrawString(g_speriod[period], ADJ(xPos + 33, isBottom), posY, BLANK);
     return (1);
 }
