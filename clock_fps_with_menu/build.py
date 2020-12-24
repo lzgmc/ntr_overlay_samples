e02353b04026ee87ad2b6bc6653febc04ac1cac7
lzgmc <lazyguymc@lzgmc.tk>
2020-12-24 02:31:27 -0800

    stuff so it builds on linux

--- clock_fps_with_menu/build.py
+++ clock_fps_with_menu/build.py
@@ -11,8 +11,8 @@ CC = "arm-none-eabi-gcc"
 CP = "arm-none-eabi-g++"
 OC = "arm-none-eabi-objcopy" 
 LD = "arm-none-eabi-ld"
-CTRULIB = '../libctru'
-DEVKITARM = 'c:/devkitPro/devkitARM'
+CTRULIB = '/opt/devkitpro/libctru'
+DEVKITARM = '/opt/devkitro/devkitARM'
 LIBPATH = '-L ./lib'
 
 def allFile(pattern):
@@ -27,7 +27,7 @@ def run(cmd):
 cwd = os.getcwd() 
 run("rm obj/*.o")
 run("rm bin/*.elf")
-run(CC+  " -s  -g -I include -I include/libntrplg " + allFile('source/libntrplg/*.c') + allFile('source/ns/*.c') + allFile('source/*.c') + allFile('source/libctru/*.c') + " -c  -march=armv6 -mlittle-endian  ");
+run(CC+  " -s  -g -I include -I include/libntrplg -I include/ctr -I /opt/devkitpro/devkitARM/arm-none-eabi/include -I /opt/devkitpro/libctru/include / " + allFile('source/libntrplg/*.c') + allFile('source/ns/*.c') + allFile('source/*.c') + allFile('source/libctru/*.c') + " -c  -march=armv6 -mlittle-endian  ");
 run(CC+"  -Os " + allFile('source/libntrplg/*.s') +  allFile('source/ns/*.s')  + allFile('source/*.s') + allFile('source/libctru/*.s') + " -c -s -march=armv6 -mlittle-endian ");
 
 run(LD + ' ' + LIBPATH + " -pie --print-gc-sections  -T 3ds.ld -Map=homebrew.map " + allFile("*.o") + " -lc -lgcc --nostdlib"  )
@@ -36,4 +36,4 @@ run("cp a.out bin/homebrew.elf ")
 run(OC+" -O binary a.out payload.bin -S")
 run("rm *.o")
 run("rm *.out")
-run('copy payload.bin ' + COPYTOPATH);
+run('cp payload.bin ' + COPYTOPATH);
